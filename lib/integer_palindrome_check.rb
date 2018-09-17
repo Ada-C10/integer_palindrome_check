c3ec26cf7174910772d5114cc7e2882333e9c729
Aurea <fu.sakura@gmail.com>
2018-09-17 16:20:08 -0700

    pass all tests

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,31 +1,23 @@
+
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
 def is_palindrome(number)
-  return false if my_phrase.nil?
-  phrase_nospaces = my_phrase.delete(' ')
-  return phrase_nospaces == reverse_array(phrase_nospaces)
+
+  return false if number.nil?
+  num_array = number.to_s.split('')
+  return num_array == reverse_array(num_array)
 end
 
 def reverse_array(arr)
 
   unless arr.nil?
     length = arr.length
-    new_arr = copy_string(arr)
+    new_arr = arr.clone
 
     (length/2).times do |i|
       tmp = new_arr[length - i - 1]
       new_arr[length - i - 1] = new_arr[i]
       new_arr[i] = tmp
     end
-  end
-
-  return new_arr
-end
-
-def copy_string(arr)
-
-  new_arr = ""
-  arr.length.times do |i|
-    new_arr += arr[i]
   end
 
   return new_arr
