cddfddca51a008d9a8f4b3735fc93938c53ea7ea
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-09-27 13:28:29 -0700

    Completed integer palindrome challenge

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,37 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
 def is_palindrome(number)
-  raise NotImplementedError
+
+  if number.nil?
+    return false
+  end
+
+  if number == ""
+    return true
+  end
+
+  # Convert to a string
+  number = number.to_s
+
+  # Return false if negative 
+  if number.include?("-")
+    return false
+  end
+
+  # Get rid of white space if white space exists
+  if number.gsub!(/\W/, "") != nil
+    number.gsub!(/\W/, "")
+  end
+
+  # Compare start/end elements
+  i = 0
+  j = number.length - 1
+  while i < number.length / 2
+
+    if number[i] != number[j]
+      return false
+    end
+    i += 1
+    j -= 1
+  end
+  return true
 end
