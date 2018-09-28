5a7595fc52e495e04895cb56ecd23fc2a6e67997
Amber Lynn <griffiam@gmail.com>
2018-09-27 21:20:17 -0700

    while check to see if an integer is a palindrome, I had to conver all multiple digit number values to a string.

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,25 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
 def is_palindrome(number)
-  raise NotImplementedError
+  # raise NotImplementedError
+  if number == nil || number < 0
+    return false
+  elsif
+    number >= 0 && number < 10
+    return true
+  else
+    start = 0
+
+    number = number.to_s
+    ender = number.length - 1
+
+    while start < ender
+      if number[start] == number[ender]
+        start += 1
+        ender -= 1
+      else
+        return false
+      end
+    end
+    return true
+  end
 end
