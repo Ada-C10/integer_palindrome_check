a443532148581afecce54989f44a3ad32ea87f4e
Addison Cole <addison.cole85@gmail.com>
2018-10-06 16:52:57 -0700

    add method to check if a number is a palindrome

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,20 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
 def is_palindrome(number)
-  raise NotImplementedError
+  if number == nil
+    return false
+  end
+  a = number.to_s
+  if number < 0
+    return false
+  end
+  y = (a.length - 1)
+  x = 0
+  until x > y do
+    if a[x] != a[y]
+      return false
+    end
+    x += 1
+    y -= 1
+  end
+  return true
 end
