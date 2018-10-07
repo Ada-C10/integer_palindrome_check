5c18dfca01e1c415cd8a124cf031f188d4445d34
Danielle Metzner <danimetzner@gmail.com>
2018-10-07 13:16:42 -0700

    Integer Palindrom complete

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,23 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
 def is_palindrome(number)
-  raise NotImplementedError
+  return false if number.class != Integer
+  number = number.to_s
+  number_dup = number.dup
+  return number == string_reverse(number_dup)
+end
+
+
+def string_reverse(my_string)
+  return my_string if my_string.class == NilClass
+  i = 0
+  j = my_string.length - 1
+  n = 0
+  while i < j
+    n = my_string[i]
+    my_string[i] = my_string[j]
+    my_string[j] = n
+    j -= 1
+    i += 1
+  end
+  return my_string
 end
