71cb672937c3cbad7160bea6fa3f81b8722df564
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-07 17:55:06 -0700

    Converting def palindrome into def is_palindrome

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,56 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
 def is_palindrome(number)
-  raise NotImplementedError
+  string_number = number.to_s
+  if number == nil
+    return false
+  elsif number < 0
+    return false
+  elsif string_number.length == 0
+    return true
+  elsif string_number.length == 1
+    return true
+  else
+    x = find_start_index(string_number, 0)
+    y = find_ending_index(string_number, string_number.length - 1)
+    start = string_number[x]
+    ending = string_number[y]
+
+    until x > y
+      if start == ending
+        x = find_start_index(string_number, x + 1)
+        start = string_number[x]
+        y = find_ending_index(string_number, y - 1)
+        ending = string_number[y]
+      else
+        return false
+      end
+    end
+    return true
+  end
+  # raise NotImplementedError
 end
+
+def find_start_index(string_number, index_val)
+  new_index = index_val
+
+  if string_number[new_index] == " "
+    until string_number[new_index] != " "
+      new_index += 1
+    end
+  end
+  return new_index
+end
+
+def find_ending_index(string_number, index_val)
+  new_index = index_val
+
+  if string_number[new_index] == " "
+    until string_number[new_index] != " "
+      new_index -= 1
+    end
+  end
+  return new_index
+end
+
+test = is_palindrome(13531)
+puts "#{test}"
--- specs/integer_palindrome_check_spec.rb
+++ specs/integer_palindrome_check_spec.rb
@@ -19,15 +19,15 @@ describe "integer palindrome check" do
 
   # check for edge cases
   describe "edge cases" do
-    # if the parameter is an object, check for nil
+  #   # if the parameter is an object, check for nil
     it "nil object is not an integer" do
       is_palindrome(nil).must_equal false
     end
-
+  #
     it "single digit palindrome" do
       is_palindrome(9).must_equal true
     end
-
+  #
     it "double digit palindrome" do
       is_palindrome(77).must_equal true
     end
@@ -35,7 +35,7 @@ describe "integer palindrome check" do
     it "negative number" do
       is_palindrome(-44).must_equal false
     end
-
+  #
     it "zero" do
       is_palindrome(0).must_equal true
     end
