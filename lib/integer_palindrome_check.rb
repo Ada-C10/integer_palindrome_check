325b9f582ad6b024d1b3eef7a00e4b1ef6066cae
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-07 17:58:07 -0700

    Deleted helper methods

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -10,16 +10,16 @@ def is_palindrome(number)
   elsif string_number.length == 1
     return true
   else
-    x = find_start_index(string_number, 0)
-    y = find_ending_index(string_number, string_number.length - 1)
+    x = 0
+    y = string_number.length - 1
     start = string_number[x]
     ending = string_number[y]
 
     until x > y
       if start == ending
-        x = find_start_index(string_number, x + 1)
+        x += 1
         start = string_number[x]
-        y = find_ending_index(string_number, y - 1)
+        y -= 1
         ending = string_number[y]
       else
         return false
@@ -30,27 +30,6 @@ def is_palindrome(number)
   # raise NotImplementedError
 end
 
-def find_start_index(string_number, index_val)
-  new_index = index_val
-
-  if string_number[new_index] == " "
-    until string_number[new_index] != " "
-      new_index += 1
-    end
-  end
-  return new_index
-end
-
-def find_ending_index(string_number, index_val)
-  new_index = index_val
-
-  if string_number[new_index] == " "
-    until string_number[new_index] != " "
-      new_index -= 1
-    end
-  end
-  return new_index
-end
 
 test = is_palindrome(13531)
 puts "#{test}"
