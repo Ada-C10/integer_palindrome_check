f77901edc2cfc151965b88d132658ffe85dc33a4
Kay <kayxn23@gmail.com>
2018-10-28 13:34:29 -0700

    added time/space complexity

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -4,12 +4,12 @@ def is_palindrome(number)
   return false if number == nil || number < 0
   return true if number == 0
 
-  reversed_num = 0
-  duplicate = number
+  reversed_num = 0  # n
+  duplicate = number # n
 
   while duplicate > 0
     reversed_num *= 10
-    last_num = duplicate % 10
+    last_num = duplicate % 10 # only contains 1 digit at a time
     reversed_num += last_num
     duplicate -= last_num
     duplicate /= 10
@@ -22,4 +22,11 @@ def is_palindrome(number)
   end
 end
 
-# 1234321
+#Time complexity: O(n) where n is the length/size of the input.
+#Depending on how many digits the number is the while loop will
+# continue to execute O(n - 1), but we drop the 1 because it is insignificant.
+
+#Space complexity: O(n) where n is the length/size of the input.
+# In this solution I defined 3 variables, 2 of which take
+#up the same space as the input's length. The last_num variable only
+#holds one digit at a time
