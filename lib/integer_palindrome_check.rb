a4b4341637bb6a679b40101c3af3a92d970d6761
Kay <kayxn23@gmail.com>
2018-10-28 12:08:02 -0700

    tests passing if converting number to array is okay

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,21 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
+require 'pry'
 def is_palindrome(number)
-  raise NotImplementedError
+  return false if number == nil || number < 0
+  string_copy = number.to_s
+  if string_copy.length == 1
+    return true
+  else
+    i = 0
+    j = string_copy.length - 1
+    while i < j
+      if string_copy[i] == string_copy[j]
+        i += 1
+        j -= 1
+      else
+        return false
+      end
+    end
+    return true
+  end
 end
