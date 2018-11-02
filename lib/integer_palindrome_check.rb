fc9dcebdf0ad25f61933a0560604856a4d8911e8
Chantelle Belic <chantelle.belic@gmail.com>
2018-11-02 11:37:22 -0700

    working method without converting int to string

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,33 +1,37 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
 require 'pry'
 
+# 1) We can compare the first digit and the last digit, then we repeat the process.
+# 2) For the first digit, we need the order of the number. Say, 12321. Dividing this by 10000 would get us the leading 1. The trailing 1 can be retrieved by taking the mod with 10.
+# 3 ) Now, to reduce this to 232.
+
 def is_palindrome(number)
 
   return false if number == nil
+  return true if !number[1]
 
-  str_num = number.to_s
-
-  return false if str_num[0] == "-"
-  return true if str_num.length < 2
-
-  temp_hash = {}
-  i = 0
-
-  while i < str_num.length do
-    if temp_hash.key? str_num[i]
-      temp_hash[str_num[i]] += 1
-    else
-      temp_hash[str_num[i]] = 1
-    end
-    i += 1
+  divisor = 1
+  while ((number / divisor) >= 10)
+    divisor *= 10
   end
 
-  odd_char = 0
-
-  temp_hash.each do |key, value|
-    if value % 2 != 0
-      odd_char += 1
+  while (number != 0)
+    first = number / divisor
+    last = number % 10
+    # If first and last digit
+    # not same return false
+    if (first != last)
+      return false
     end
+
+    # Removing the leading and
+    # trailing digit from number
+    number = (number % divisor)/10
+    # Reducing divisor by a factor
+    # of 2 as 2 digits are dropped
+    divisor = divisor / 100
   end
-  return odd_char > 1 ? false : true
+
+  return true
+
 end
